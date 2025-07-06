e782465cb25c3bff693fcb3c7b3eea7af0626196
begaiym-mm <127165825+begaiym-mm@users.noreply.github.com>
2025-07-06 22:32:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,48 @@
+import os
+import random
+import schedule
+import time
+from telegram import Bot
+from telegram.error import TelegramError
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+bot = Bot(token=TOKEN)
+
+quiz_data = [
+    {
+        "question": "Какой язык программирования используется для Telegram-ботов?",
+        "options": ["Python", "HTML", "CSS", "Photoshop"],
+        "correct_option_id": 0
+    },
+    {
+        "question": "Сколько дней в високосном году?",
+        "options": ["365", "366", "364", "360"],
+        "correct_option_id": 1
+    }
+]
+
 def send_daily_quiz():
+    quiz = random.choice(quiz_data)
     try:
-        bot.send_message(chat_id=CHANNEL_ID, text="🔔 Привет! Бот работает!")
-        print("✅ Сообщение отправлено.")
+        bot.send_poll(
+            chat_id=CHANNEL_ID,
+            question=quiz["question"],
+            options=quiz["options"],
+            type='quiz',
+            correct_option_id=quiz["correct_option_id"],
+            is_anonymous=False
+        )
+        print("✅ Куиз отправлен.")
     except TelegramError as e:
-        print(f"❌ Ошибка: {e}")
+        print(f"❌ Ошибка отправки: {e}")
+
+send_daily_quiz()
+#schedule.every().day.at("17:20").do(send_daily_quiz)
+
+print("👀 Бот запущен на Heroku")
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
